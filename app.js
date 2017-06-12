390300064bd057ceee833a777886648e24d41de3
Nicholas Noochla-or <nnlaor@gmail.com>
2017-06-11 19:45:52 -0700

    Create app.js

--- app.js
+++ app.js
@@ -7,18 +7,14 @@ var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var user = require('./routes/user');
-var test = require('./routes/test');
-
+var index = require('./controllers/controller.js');
 
 var app = express();
 
-mongoose.connect('localhost:27017/duckuckScrape');
-// require('./config/passport');
+mongoose.connect('127.0.0.1:27017/duckuckScrape');
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.engine('.hbs', expressHbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
@@ -29,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
 app.use('/', index);
 
 // catch 404 and forward to error handler
